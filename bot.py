b61171eafcbd1c340cb8974e633f7390562ab128
egorkasgv <164521961+egorkasgv@users.noreply.github.com>
2024-10-30 11:05:53 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
-API_TOKEN = '7671080534:AAHmuEYUhZeAUSOS_U_CDpDDFzViOLmXCgo'
+API_TOKEN = ''
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
